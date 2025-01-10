a279ebf9b732ae5ae7a67e2f1652d122907481ed
lauraleecallahan <lauraleecallahan@outlook.com>
2025-01-09 22:51:44 -0600

    Delete python-api-challenge directory

--- python-api-challenge/VacationPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "af0fcf7f0471a703c3b7168b79464fbc"
-
-# Geoapify API Key
-geoapify_key = "8cf9a982d76a489a8056204466e19f09"
--- python-api-challenge/WeatherPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "af0fcf7f0471a703c3b7168b79464fbc"
-
-# Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
